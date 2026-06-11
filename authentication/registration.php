cf2feb280ca3dad12a8cb2eff1e485e9950fde61
Vighnesh <vighneshvtawade1605@gmail.com>
2026-06-12 01:26:29 +0530

    student registration done-- Username remains

--- authentication/registration.php
+++ authentication/registration.php
@@ -5,6 +5,7 @@ include '../config/connect.php';
 if(isset($_POST['c_submit'])){
     $name = $_POST['c_name'];
     $pass = $_POST['c_pass'];
+    $loginfor = $_POST['loginfor'];
 
     $hashpass = password_hash($pass, PASSWORD_DEFAULT);
 
@@ -15,7 +16,7 @@ if(isset($_POST['c_submit'])){
     // else{
     //     echo "not recorded";
     // }
-
+    if($loginfor == 'College'){
     $sql =$conn->prepare( "SELECT password FROM college where name = ?");
     $sql->bind_param("s",$name);
     $sql->execute();
@@ -38,4 +39,30 @@ if(isset($_POST['c_submit'])){
     }
   $sql->close();  
 
+}
+//TO assign
+//  if($loginfor == 'Admin'){
+//     $sql =$conn->prepare( "SELECT password FROM ..... where name = ?");
+//     $sql->bind_param("s",$name);
+//     $sql->execute();
+
+//     $result = $sql->get_result();
+
+//     if($row = $result->fetch_assoc()){
+//          $dbpass = $row['password'];
+
+//          if (password_verify($pass, $dbpass)) {
+//             header("Location: ");
+//         } else {
+//             echo "Invalid password.";
+//         }
+
+    
+//     }
+//     else{
+//         echo "Username not found";
+//     }
+//   $sql->close();  
+
+// }
 }

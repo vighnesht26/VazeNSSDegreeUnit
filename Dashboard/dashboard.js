bb2c35653a9f71a8af023be6e42c86047f7b5c2c
Vighnesh <vighneshvtawade1605@gmail.com>
2026-06-25 22:37:05 +0530

    added sidebar and closable on mobile functinality added

---
+++ Dashboard/dashboard.js
@@ -0,0 +1,16 @@
+
+
+function toggleSidebar(element){
+    const sidebar = document.getElementById('sidebar');
+   
+        if(element.id === "open_sidebar"){
+            sidebar.classList.remove("-translate-x-full");
+            sidebar.classList.add("translate-x-0");
+            
+
+        }
+        else if(element.id ==="close_sidebar"){
+            sidebar.classList.remove("translate-x-0");
+             sidebar.classList.add("-translate-x-full");
+        }
+}

2ad178abf3d5607faa3cd9992402a52572486996
Vighnesh <vighneshvtawade1605@gmail.com>
2026-08-16 14:16:28 +0530

    Comment out google_config.php

--- api/drive_uploader.php
+++ api/drive_uploader.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 include '../vendor/autoload.php';
-include './google_config.php';
+// include './google_config.php';
 
 function getGoogleDriveService(){
     $client = new Google\Client();

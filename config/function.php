a70589f1143401dbd3189b3a3a74337b67a34735
vighnesht26 <vighneshvtawade1605@gmail.com>
2026-09-03 23:40:03 +0530

    Update function.php

--- config/function.php
+++ config/function.php
@@ -2,7 +2,7 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require_once __DIR__ . '/../api/mail-config.php';
+// require_once __DIR__ . '/../api/mail-config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
